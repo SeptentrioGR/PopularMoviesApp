03ca492c676dbc20f471d48091a24e3d03134e7e
Petros Kitsaras <Jackriderpk@gmail.com>
2017-02-13 13:53:06 +0200

    Fixed by change utilities.java to Utilities.java

---
+++ app/src/main/java/com/udacity/projectpopularmovies/Utils/Utilities.java
@@ -0,0 +1,23 @@
+package com.udacity.projectpopularmovies.Utils;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+public class Utilities {
+    public static Toast myToast;
+    //This is used to make Toast
+    public static void MakeToast(Context context, String message, int length){
+        if(myToast!=null){
+            myToast.cancel();
+        }else{
+            myToast.makeText(context,message,length).show();
+        }
+    }
+
+    public static void DebugLog(String TAG,String message){
+        Log.i(TAG,message);
+    }
+
+
+}
